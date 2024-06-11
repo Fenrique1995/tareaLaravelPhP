489c56ee73436c12a2f87a2f1014a9a362e2bab8
unknown <fenrique310@gmail.com>
2024-06-11 02:04:14 -0300

    creacion de vista, barra de navegacion y como usar el route en laravel

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome');//aca puedo definir que se va a mostrar
+});
+
+/*Route::get('/saludo', function () {//el /saludo es otra ruta
+    return view('app');//aca puedo definir que se va a mostrar
+});*/
+
+Route::get('/todos', function () {//el /saludo es otra ruta
+    return view('tareas.index');//aca puedo definir que se va a mostrar
 });
